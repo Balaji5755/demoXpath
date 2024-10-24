b2f3fadba3b3d4772365103a1da9aa09ef521b81
Balaji5755 <137764386+Balaji5755@users.noreply.github.com>
2024-10-24 14:16:48 +0530

    Initial commit

---
+++ Demoxpath.java
@@ -0,0 +1,31 @@
+package demoXpath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demoxpath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Chrome113\\chromedriver.exe");
+		WebDriver d = new ChromeDriver();
+		
+		
+		d.get("https://www.facebook.com/");
+		/*
+		d.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("balajiyadhev@gmail.com");
+		d.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("Sunny");
+		d.findElement(By.xpath("//*[@id=\"u_0_5_E/\"]")).click();
+		*/
+		
+		d.findElement(By.xpath("//a[@data-testid='royal_login_button]")).click();
+		
+		
+		
+		
+
+	}
+
+}
